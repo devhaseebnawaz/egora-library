7991e7458568bf72b48f4143dfa6376e5f127ed3
Mahum Maqbool <mahummaqbool@egora.pk>
2025-06-24 12:35:59 +0500

    Added the icons again

--- dist/components/brgr/header/Header.js
+++ dist/components/brgr/header/Header.js
@@ -136,7 +136,7 @@ function CustomNavbar(_ref) {
       backgroundColor: "#121212"
     }
   }, /*#__PURE__*/_react["default"].createElement("img", {
-    src: prop.editable.logoImage,
+    src: "".concat(prop.editable.logoImage),
     alt: "BRGR Logo",
     style: {
       width: "100%",
