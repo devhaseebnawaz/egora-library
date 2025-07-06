cb23ef48123b4f4a22a4455d1a2cb79dec3d02e4
Mahum Maqbool <mahummaqbool@egora.pk>
2025-07-06 18:19:31 +0500

    made a change

--- src/components/brgr/header/Header.js
+++ src/components/brgr/header/Header.js
@@ -15,7 +15,7 @@ export default function CustomNavbar({ themeColors, actions, prop, styles, state
         <AppBar
             position="static"
             style={{
-                backgroundColor: themeColors?.AppBarBackgroundColor?.value ? themeColors?.AppBarBackgroundColor?.value :  styles?.AppBarBackgroundColor?.value != "" ?  styles?.AppBarBackgroundColor?.value : "#121212",
+                backgroundColor:  styles?.AppBarBackgroundColor?.value != "" ?  styles?.AppBarBackgroundColor?.value : themeColors?.AppBarBackgroundColor?.value ? themeColors?.AppBarBackgroundColor?.value : "#121212",
                 boxShadow: "none",
                 position: "relative",
                 zIndex: 10,
