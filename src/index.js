617a8fd0110f757474b60a9eeecb82c4cfc00c84
Mahum Maqbool <mahummaqbool@egora.pk>
2025-07-12 20:34:14 +0500

    Changed Location modal

--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ import AllCategoriesPage from './components/brgr/categories/AllCategoriesPage';
 import InfoBlock from './components/brgr/InfoBlock/InfoBlock';
 import Footer from './components/brgr/footer/Footer';
 import CategoryCarousel from './components/brgr/categories/CategoryCarousel';
+import LocationModal from './components/brgr/categories/locationModal';
 
-
-export { Header, HeroCarousel, CategoryCarousel, SearchBar, PopularMenuSection, AllCategoriesPage, Footer, InfoBlock };
+export { Header, LocationModal, HeroCarousel, CategoryCarousel, SearchBar, PopularMenuSection, AllCategoriesPage, Footer, InfoBlock };
