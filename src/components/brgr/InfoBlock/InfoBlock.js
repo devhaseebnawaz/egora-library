3247d4229734ac815aa1e05163965380e8adba63
Mahum Maqbool <mahummaqbool@egora.pk>
2025-08-04 06:45:42 +0500

    EG-1171 Added styles

--- src/components/brgr/InfoBlock/InfoBlock.js
+++ src/components/brgr/InfoBlock/InfoBlock.js
@@ -1,7 +1,67 @@
 import React from 'react';
 import { Box, Typography } from '@mui/material';
 
-export default function InfoBlock({ themeColors, actions, prop, styles, states }) {
+export default function InfoBlock({ themeColors, actions, prop, styles, states, globalComponentStyles }) {
+
+    const getInfoBlockTitleStyles = {
+        color:
+            styles?.InfoBlockTitleColor?.value !== ""
+                ? styles?.InfoBlockTitleColor?.value
+                : globalComponentStyles?.Text?.color?.value !== ""
+                    ? globalComponentStyles?.Text?.color?.value
+                    : themeColors?.InfoBlockTitleColor?.value,
+    
+        fontSize:
+            styles?.InfoBlockTitleSize?.value != 0
+                ? styles?.InfoBlockTitleSize?.value
+                : globalComponentStyles?.Text?.size?.value != 0
+                    ? globalComponentStyles?.Text?.size?.value
+                    : themeColors?.InfoBlockTitleSize?.value,
+    
+        fontFamily:
+            styles?.InfoBlockTitleFont?.value !== ""
+                ? styles?.InfoBlockTitleFont?.value
+                : globalComponentStyles?.Text?.fontFamily?.value !== ""
+                    ? globalComponentStyles?.Text?.fontFamily?.value
+                    : themeColors?.InfoBlockTitleFont?.value,
+    
+        fontStyle:
+            styles?.InfoBlockTitleStyle?.value !== ""
+                ? styles?.InfoBlockTitleStyle?.value
+                : globalComponentStyles?.Text?.fontWeight?.value !== ""
+                    ? globalComponentStyles?.Text?.fontWeight?.value
+                    : themeColors?.InfoBlockTitleStyle?.value,
+    };
+
+    const getInfoBlockDescriptionStyles = {
+        color:
+            styles?.InfoBlockDescriptionColor?.value !== ""
+                ? styles?.InfoBlockDescriptionColor?.value
+                : globalComponentStyles?.Text?.color?.value !== ""
+                    ? globalComponentStyles?.Text?.color?.value
+                    : themeColors?.InfoBlockDescriptionColor?.value,
+    
+        fontSize:
+            styles?.InfoBlockDescriptionSize?.value != 0
+                ? styles?.InfoBlockDescriptionSize?.value
+                : globalComponentStyles?.Text?.size?.value != 0
+                    ? globalComponentStyles?.Text?.size?.value
+                    : themeColors?.InfoBlockDescriptionSize?.value,
+    
+        fontFamily:
+            styles?.InfoBlockDescriptionFont?.value !== ""
+                ? styles?.InfoBlockDescriptionFont?.value
+                : globalComponentStyles?.Text?.fontFamily?.value !== ""
+                    ? globalComponentStyles?.Text?.fontFamily?.value
+                    : themeColors?.InfoBlockDescriptionFont?.value,
+    
+        fontStyle:
+            styles?.InfoBlockDescriptionStyle?.value !== ""
+                ? styles?.InfoBlockDescriptionStyle?.value
+                : globalComponentStyles?.Text?.fontWeight?.value !== ""
+                    ? globalComponentStyles?.Text?.fontWeight?.value
+                    : themeColors?.InfoBlockDescriptionStyle?.value,
+    };
 
     return (
         <Box
@@ -9,7 +69,6 @@ export default function InfoBlock({ themeColors, actions, prop, styles, states }
                 width: '100%',
                 maxWidth: '935px',
                 padding: '50px 0',
-                color: styles?.InfoBlockTitleColor?.value != "" ? styles?.InfoBlockTitleColor?.value : themeColors?.InfoBlockTitleColor?.value,
                 fontWeight: '500 !important',
                 fontSize: '14px',
                 margin: '0 auto',
@@ -18,16 +77,16 @@ export default function InfoBlock({ themeColors, actions, prop, styles, states }
                 display: 'flex',
                 flexDirection: 'column',
                 alignItems: 'center',
+                backgroundColor: styles?.InfoBlockBackgroundColor?.value != ""
+                    ? styles?.InfoBlockBackgroundColor?.value
+                    : themeColors?.InfoBlockBackgroundColor?.value,
             }}
         >
-            <Typography style={{ fontWeight: '300', fontSize: '2rem' }}>
+            <Typography  sx={{...getInfoBlockTitleStyles}}>
                 {prop.editable.title.value}
             </Typography>
 
-            <Typography style={{
-                color: styles?.InfoBlockDescriptionColor?.value != "" ? styles?.InfoBlockDescriptionColor?.value : themeColors?.InfoBlockTitleColor?.value,
-                fontWeight: '500 ', fontSize: '14px'
-            }}>
+            <Typography sx={{...getInfoBlockDescriptionStyles}} >
                 {prop.editable.description.value}
             </Typography>
         </Box>
--- src/components/brgr/textBlock/textBlock.js
+++ src/components/brgr/textBlock/textBlock.js
@@ -1,7 +1,37 @@
 import React from 'react';
 import { Box, Typography } from '@mui/material';
 
-export default function TextBlock({ themeColors, actions, prop, styles, states }) {
+export default function TextBlock({ themeColors, actions, prop, styles, states, globalComponentStyles }) {
+
+    const getTextBlockStyles = {
+        color:
+            styles?.TextBlockTextColor?.value !== ""
+                ? styles?.TextBlockTextColor?.value
+                : globalComponentStyles?.Text?.color?.value !== ""
+                    ? globalComponentStyles?.Text?.color?.value
+                    : themeColors?.TextBlockTextColor?.value,
+    
+        fontSize:
+            styles?.TextBlockTextSize?.value != 0
+                ? styles?.TextBlockTextSize?.value
+                : globalComponentStyles?.Text?.size?.value != 0
+                    ? globalComponentStyles?.Text?.size?.value
+                    : themeColors?.TextBlockTextSize?.value,
+    
+        fontFamily:
+            styles?.TextBlockTextFont?.value !== ""
+                ? styles?.TextBlockTextFont?.value
+                : globalComponentStyles?.Text?.fontFamily?.value !== ""
+                    ? globalComponentStyles?.Text?.fontFamily?.value
+                    : themeColors?.TextBlockTextFont?.value,
+    
+        fontStyle:
+            styles?.TextBlockTextStyle?.value !== ""
+                ? styles?.TextBlockTextStyle?.value
+                : globalComponentStyles?.Text?.fontWeight?.value !== ""
+                    ? globalComponentStyles?.Text?.fontWeight?.value
+                    : themeColors?.TextBlockTextStyle?.value,
+    };
 
     return (
         <Box
@@ -9,7 +39,6 @@ export default function TextBlock({ themeColors, actions, prop, styles, states }
                 width: '100%',
                 maxWidth: '935px',
                 padding: '50px 0',
-                color: styles?.TextBlockTextColor?.value != "" ? styles?.TextBlockTextColor?.value : themeColors?.TextBlockTextColor?.value,
                 fontWeight: '500 !important',
                 fontSize: '14px',
                 margin: '0 auto',
@@ -18,9 +47,12 @@ export default function TextBlock({ themeColors, actions, prop, styles, states }
                 display: 'flex',
                 flexDirection: 'column',
                 alignItems: 'center',
+                backgroundColor: styles?.TextBlockBackgroundColor?.value != ""
+                    ? styles?.TextBlockBackgroundColor?.value
+                    : themeColors?.TextBlockBackgroundColor?.value,
             }}
         >
-            <Typography >
+            <Typography sx={{...getTextBlockStyles}} >
                 {prop.editable.text.value}
             </Typography>
         </Box>
