06f25e2f4f5635552d5c94b933d1bf4baa1d5653
sufyan <msufyan@egora.pk>
2025-08-05 10:02:22 +0500

    change universal img

--- src/UniversalImage.js
+++ src/UniversalImage.js
@@ -1,13 +1,45 @@
-import React from 'react';
+// import React from 'react';
 
-let NextImage;
-try {
-  NextImage = require('next/image').default;
-} catch (e) {
-  NextImage = null;
-}
 
-export default function UniversalImage({ src, alt = "image", width = '100%', height = '100%', objectFit = 'contain', style = {} }) {
+// let NextImage;
+// try {
+//   NextImage = require('next/image').default;
+// } catch (e) {
+//   NextImage = null;
+// }
+
+// export default function UniversalImage({ src, alt = "image", width = '100%', height = '100%', objectFit = 'contain', style = {} }) {
+//   const commonStyle = {
+//     width,
+//     height,
+//     objectFit,
+//     ...style,
+//   };
+
+//   if (NextImage) {
+//     return (
+//       <div style={{ position: 'relative', width, height }}>
+//         <NextImage src={src} alt={alt} layout="fill" objectFit={objectFit} />
+//       </div>
+//     );
+//   }
+
+//   return <img src={src} alt={alt} style={commonStyle} />;
+// }
+
+
+import React from "react";
+import Image from "next/image"; // Will work only in Next.js
+
+export default function UniversalImage({
+  src,
+  alt = "image",
+  width = "100%",
+  height = "100%",
+  objectFit = "contain",
+  style = {},
+  fallback = false, // Pass true to force <img> usage
+}) {
   const commonStyle = {
     width,
     height,
@@ -15,13 +47,19 @@ export default function UniversalImage({ src, alt = "image", width = '100%', hei
     ...style,
   };
 
-  if (NextImage) {
-    return (
-      <div style={{ position: 'relative', width, height }}>
-        <NextImage src={src} alt={alt} layout="fill" objectFit={objectFit} />
-      </div>
-    );
+  // If you want to use <img> instead of next/image in some cases
+  if (fallback) {
+    return <img src={src} alt={alt} style={commonStyle} />;
   }
 
-  return <img src={src} alt={alt} style={commonStyle} />;
+  return (
+    <div style={{ position: "relative", width, height }}>
+      <Image
+        src={src}
+        alt={alt}
+        fill
+        style={{ objectFit }}
+      />
+    </div>
+  );
 }
