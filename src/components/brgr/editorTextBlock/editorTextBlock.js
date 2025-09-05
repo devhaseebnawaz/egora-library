5c3ae3358b7cce2a7fda8a17dc28abdff8da38c8
sufyan <msufyan@egora.pk>
2025-09-05 10:49:23 +0500

    add markdown

--- src/components/brgr/editorTextBlock/editorTextBlock.js
+++ src/components/brgr/editorTextBlock/editorTextBlock.js
@@ -1,30 +1,28 @@
 import React from 'react';
 import { Box, Typography } from '@mui/material';
+import Markdown from '../../markdown';
 
 export default function EditorTextBlock({ themeColors, actions, prop, styles, states, globalComponentStyles }) {
 
-    console.log('prop',prop);
-    
-
     return (
         <Box
-            style={{
+            sx={{
                 width: '100%',
-                padding: '50px 0',
+                py: '50px',
+                px: {
+                    xs: '12px',
+                    sm: '20px',
+                    md: '40px',
+                },
                 fontSize: '14px',
                 margin: '0 auto',
-                height: '345px',
                 overflowY: 'hidden',
                 display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                backgroundColor: styles?.EditorTextBlockBackgroundColor?.value != ""
-                    ? styles?.EditorTextBlockBackgroundColor?.value
-                    : themeColors?.EditorTextBlockBackgroundColor?.value,
+                backgroundColor: styles?.EditorTextBlockBackgroundColor?.value || themeColors?.EditorTextBlockBackgroundColor?.value,
             }}
         >
             <Typography>
-                {prop.editable.text.value}
+                <Markdown children={prop.editable.text.value} />
             </Typography>
         </Box>
     );
